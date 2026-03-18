81fc964de8071b52ae01b6bcfab21ae1a742d487
Vikas Gowda J A <74110397+vikasgowdaja@users.noreply.github.com>
2026-03-18 13:03:09 +0530

    fix cors   .cors(cors -> cors.configurationSource(corsConfigurationSource()))

--- server/src/main/java/com/SpringReactJS/PracticeJavaSpringReactJS/config/SecurityConfig.java
+++ server/src/main/java/com/SpringReactJS/PracticeJavaSpringReactJS/config/SecurityConfig.java
@@ -23,8 +23,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .cors()
-            .and()
+            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
             .csrf().disable()
             .authorizeHttpRequests(authorize -> authorize
                     .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
